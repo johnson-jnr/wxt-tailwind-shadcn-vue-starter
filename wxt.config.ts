613936fb5657e29f49f65a6341422fdd6a404a1e
Johnson Towoju <johnsontodimu@gmail.com>
2026-04-13 18:11:26 +0100

    feat: setup shadcn-vue and add button component

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'wxt';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'node:path';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
     modules: ['@wxt-dev/module-vue'],
     vite: () => ({
-        plugins: [tailwindcss()]
+        plugins: [tailwindcss()],
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, '.'),
+            }
+        }
     })
 });
