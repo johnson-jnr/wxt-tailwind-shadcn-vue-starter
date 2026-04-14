34d20ec016ccfe9ad36f5c90b0334675802f921d
Johnson Towoju <johnsontodimu@gmail.com>
2026-04-14 11:19:11 +0100

    fix: typo in ext desc

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     srcDir: 'src',
     manifest: {
         name: 'WXT Starter Template',
-        description: 'A simple WXT starter template preconfigued with Vue, Tailwind and Shadcn-Vue'
+        description: 'A simple WXT starter template preconfigured with Vue, Tailwind and Shadcn-Vue'
     },
     modules: ['@wxt-dev/module-vue'],
     vite: () => ({
