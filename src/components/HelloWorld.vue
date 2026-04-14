3c0c71d21b01c2ab5433c150eb2146af32ef5608
Johnson Towoju <johnsontodimu@gmail.com>
2026-04-14 11:11:07 +0100

    cleanup and minor updates

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+import Button from './shadcn/button/Button.vue';
+
+defineProps({
+    msg: String,
+});
+
+const count = ref(0);
+</script>
+
+<template>
+    <div class="space-y-2">
+        <p class="text-2xl">{{ msg }}</p>
+        <Button variant="outline" @click="count++">count is {{ count }}</Button>
+    </div>
+</template>
+
+<style scoped>
+</style>
--- wxt.config.ts
+++ wxt.config.ts
@@ -3,6 +3,10 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     srcDir: 'src',
+    manifest: {
+        name: 'WXT Starter Template',
+        description: 'A simple WXT starter template preconfigued with Vue, Tailwind and Shadcn-Vue'
+    },
     modules: ['@wxt-dev/module-vue'],
     vite: () => ({
         plugins: [tailwindcss()]
